0080d1488bc2c199d34dad135c0982600b3ed371
yichien <kevinyichienlee@gmail.com>
2024-07-10 22:31:42 +0800

    multiple test structures

--- src/test/Util.test.ts
+++ src/test/Util.test.ts
@@ -13,35 +13,45 @@ describe("Util test suite", () => {
     expect(actual).toBe(expected);
   });
 
-  it("should return infor for valid string", () => {
-    const actual = getStringInfo("My-String");
+  describe("getStringInfo for arg My-String should", () => {
+    test("return right length", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.characters).toHaveLength(9);
+    });
 
-    expect(actual.lowerCase).toBe("my-string");
-    expect(actual.extraInfo).toEqual({});
+    test("return right lowerCase", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.lowerCase).toBe("my-string");
+    });
 
-    expect(actual.characters.length).toBe(9);
-    expect(actual.characters).toHaveLength(9);
+    test("return right upperCase", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.upperCase).toBe("MY-STRING");
+    });
 
-    expect(actual.characters).toEqual([
-      "M",
-      "y",
-      "-",
-      "S",
-      "t",
-      "r",
-      "i",
-      "n",
-      "g",
-    ]);
-    expect(actual.characters).toContain<string>("M");
+    test("return right characters", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.characters).toEqual([
+        "M",
+        "y",
+        "-",
+        "S",
+        "t",
+        "r",
+        "i",
+        "n",
+        "g",
+      ]);
+    });
 
-    expect(actual.characters).toEqual(
-      expect.arrayContaining(["S", "t", "r", "i", "n", "g"])
-    );
+    test("return right extraInfo", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.extraInfo).not.toBeUndefined();
+    });
 
-    expect(actual.extraInfo).not.toBe(undefined);
-    expect(actual.extraInfo).not.toBeUndefined();
-    expect(actual.extraInfo).toBeDefined();
-    expect(actual.extraInfo).toBeTruthy(); // when you are not sure about the value
+    test("return right extraInfo", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.extraInfo).toEqual({});
+    });
   });
 });
