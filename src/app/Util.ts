fa336c5447a34028e9fb8700819246c3e2348599
yichien <kevinyichienlee@gmail.com>
2024-07-10 22:23:37 +0800

    Jest assertions and matchers

--- src/app/Util.ts
+++ src/app/Util.ts
@@ -1,3 +1,21 @@
 export function toUpperCase(arg: string): string {
   return arg.toUpperCase();
 }
+
+export type stringInfo = {
+  lowerCase: string;
+  upperCase: string;
+  characters: string[];
+  length: number;
+  extraInfo: Object | undefined;
+};
+
+export function getStringInfo(arg: string): stringInfo {
+  return {
+    lowerCase: arg.toLowerCase(),
+    upperCase: arg.toUpperCase(),
+    characters: arg.split(""),
+    length: arg.length,
+    extraInfo: {},
+  };
+}
--- src/test/Util.test.ts
+++ src/test/Util.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Util";
+import { getStringInfo, toUpperCase } from "../app/Util";
 
 describe("Util test suite", () => {
   it("shoud return uppercase of valid string", () => {
@@ -11,5 +11,37 @@ describe("Util test suite", () => {
 
     // assert:
     expect(actual).toBe(expected);
+  });
+
+  it("should return infor for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+    expect(actual.characters).toContain<string>("M");
+
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["S", "t", "r", "i", "n", "g"])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy(); // when you are not sure about the value
   });
 });
