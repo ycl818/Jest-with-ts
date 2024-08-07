ddba0e03dd830fe6244aca0709109d431941617d
yichien <kevinyichienlee@gmail.com>
2024-08-07 14:34:27 +0800

    password checker setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from "@jest/types";
 
+const baseDir = "<rootDir>/src/app/pass_checker";
+const baseTestDir = "<rootDir>/src/test/pass_checker";
+
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true, // Display mor information in console
   collectCoverage: true, // Collect coverage information
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"], // Files to collect coverage information from
+  collectCoverageFrom: [`${baseDir}/**/*.ts`], // Files to collect coverage information from
+  testMatch: [`${baseTestDir}/**/*.test.ts`], // Files to run tests from
 };
 
 export default config;
