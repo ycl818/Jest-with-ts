53881879806311582d1c05023c0a5dbcaf3f28be
yichien <kevinyichienlee@gmail.com>
2024-08-07 18:27:35 +0800

    password checker iteration 3

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -46,4 +46,16 @@ describe("PasswordChecker test suite", () => {
         expect(actual.reasons).toHaveLength(0);
         expect(actual.valid).toBe(true);
     });
+
+    it("Admin password with no number is invalid", () => {
+        const actual = sut.checkAdminPassword("ABCDabcd");
+        expect(actual.reasons).toContain(PasswordErrors.NO_NUMBER);
+        expect(actual.valid).toBe(false);
+    });
+
+    it("Admin password with number is valid", () => {
+        const actual = sut.checkAdminPassword("ABCDabcd4564");
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_NUMBER);
+        expect(actual.valid).toBe(true);
+    });
 });
