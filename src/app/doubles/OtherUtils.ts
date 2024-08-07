dee42f53e3fb76fe41ebb8f59934964d57dbc5d1
yichien <kevinyichienlee@gmail.com>
2024-08-07 21:39:16 +0800

    fakes

--- src/app/doubles/OtherUtils.ts
+++ src/app/doubles/OtherUtils.ts
@@ -6,6 +6,20 @@ export type stringInfo = {
     extraInfo: Object | undefined;
 };
 
+type LoggerServiceCallback = (arg: string) => void;
+
 export function calculateComplexity(stringInfo: stringInfo) {
     return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+}
+
+export function toUpperCaseWithCb(
+    arg: string,
+    callback: LoggerServiceCallback
+) {
+    if (!arg) {
+        callback("Invalid argument");
+        return;
+    }
+    callback(`called function with ${arg}`);
+    return arg.toUpperCase();
 }
